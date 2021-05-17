c5add6fad0898e7266e11836fba0c56bdb2d3c05
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2021-05-17 17:37:57 -0500

    inicial pdf

--- src/app/documents/document.module.ts
+++ src/app/documents/document.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+
 import { DocumentRoutingModule } from './document-routing.module';
 import { DocutestComponent } from './docutest/docutest.component';
 
@@ -11,7 +17,12 @@ import { DocutestComponent } from './docutest/docutest.component';
   ],
   imports: [
     CommonModule,
-    DocumentRoutingModule
+    DocumentRoutingModule,
+    NzCardModule,
+    NzButtonModule,
+    NzInputModule,
+    NzFormModule,
+    NzTabsModule
   ]
 })
 export class DocumentModule { }
--- src/app/documents/docutest/docutest.component.ts
+++ src/app/documents/docutest/docutest.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-docutest',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocutestComponent implements OnInit {
 
+  index1 = 0;
+  index2 = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generatePDF(): void {
+    const docDefinition = {
+      header: 'C#Corner PDF Header',
+      content: 'Sample PDF generated with Angular and PDFMake for C#Corner Blog'
+    };
+    pdfMake.createPdf(docDefinition).open();
   }
 
 }
