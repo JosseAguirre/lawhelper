595af361fb1e58b727c71a6458792d94109a3550
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2021-08-17 18:48:35 -0500

    fix pdf generator

--- src/app/documents/document.module.ts
+++ src/app/documents/document.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// PDF
+import { PdfMakeWrapper } from 'pdfmake-wrapper';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+PdfMakeWrapper.setFonts(pdfFonts);
+
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMessageModule } from 'ng-zorro-antd/message';
 
 import { DocumentRoutingModule } from './document-routing.module';
 import { DocutestComponent } from './docutest/docutest.component';
@@ -25,7 +35,11 @@ import { DocutestComponent } from './docutest/docutest.component';
     NzButtonModule,
     NzInputModule,
     NzFormModule,
-    NzTabsModule
+    NzTabsModule,
+    NzTableModule,
+    NzDividerModule,
+    NzIconModule,
+    NzMessageModule
   ]
 })
 export class DocumentModule { }
--- src/app/documents/docutest/docutest.component.ts
+++ src/app/documents/docutest/docutest.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
-import * as pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { PdfMakeWrapper, Txt, Columns, Table, QR, Ul, Item } from 'pdfmake-wrapper';
 
-(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+class Product{
+  product!: string;
+  price!: number;
+  quantity!: number;
+}
+
+class Invioce{
+  name!: string;
+  address!: string;
+  number!: string;
+  email!: string;
+
+  products: Product[] = [];
+  details!: string;
+
+  constructor() {
+    this.products.push(new Product());
+  }
+}
 
 @Component({
   selector: 'app-docutest',
@@ -13,18 +30,22 @@ import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 })
 export class DocutestComponent implements OnInit {
 
+  invoice = new Invioce();
   validateForm!: FormGroup;
   index1 = 0;
-  index2 = 0;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private message: NzMessageService) { }
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
-      nombre: [null, [Validators.required]],
+      name: [null, [Validators.required]],
       email: [null, [Validators.email, Validators.required]],
-      direccion: [null, [Validators.required]],
-      numero: [null, [Validators.minLength(10), Validators.maxLength(10), Validators.required]]
+      address: [null, [Validators.required]],
+      number: [null, [Validators.minLength(10), Validators.maxLength(10), Validators.required]],
+      product: [null, [Validators.required]],
+      price: [null, [Validators.required, Validators.min(0)]],
+      quantity: [null, [Validators.required, Validators.min(0)]],
+      details: [null, [Validators.maxLength(100)]]
     });
   }
 
@@ -37,11 +58,103 @@ export class DocutestComponent implements OnInit {
   }
 
   generatePDF(): void {
-    const docDefinition = {
-      header: 'C#Corner PDF Header',
-      content: 'Sample PDF generated with Angular and PDFMake for C#Corner Blog'
-    };
-    pdfMake.createPdf(docDefinition).open();
+    this.submitForm();
+    const pdf = new PdfMakeWrapper();
+    try {
+
+      pdf.add(
+        new Txt('Documento de prueba N°1').alignment('center').fontSize(16).color('#047886').end
+      );
+
+      pdf.add(
+        new Txt('Factura').fontSize(20).bold().alignment('center').decoration('underline').color('skyblue').end
+      );
+
+      pdf.add(
+        new Txt('Detalles del cliente').style('sectionHeader').end
+      );
+
+      pdf.add(
+        new Columns(
+        [
+          [
+            this.invoice.name,
+            this.invoice.address,
+            this.invoice.email,
+            this.invoice.number
+          ],
+          [
+            `Fecha: ${new Date().toLocaleString()}`,
+            `Factura No : ${((Math.random() * 1000).toFixed(0))}`
+          ]
+        ]
+        ).alignment('right').end
+      );
+
+      pdf.add(
+        new Txt('Detalle').style('sectionHeader').end
+      );
+
+      pdf.add(
+        new Table([
+          ['Product', 'Price', 'Quantity', 'Amount'],
+          ...this.invoice.products.map(p => ([p.product, p.price, p.quantity, (p.price * p.quantity).toFixed(2)])),
+          [
+            {text: 'Total Amount', colSpan: 3}, {}, {}, this.invoice.products.reduce((sum, p) => sum + (p.quantity * p.price), 0).toFixed(2)
+          ]
+        ]).headerRows(1).widths(['*', 'auto', 'auto', 'auto']).end
+      );
+
+      pdf.add(
+        new Txt('Detalles adicionales').style('sectionHeader').end
+      );
+
+      pdf.add(
+        new Txt(this.invoice.details).margin([0, 0 , 0, 15]).end
+      );
+
+      const signature = new Txt('Firma').alignment('right').italics();
+
+      pdf.add(
+        new QR(this.invoice.name).fit(50).end
+      );
+
+      pdf.add(
+        new Ul([
+          '__________________________________',
+          'Firma',
+        ]).bold().end
+      );
+
+      pdf.add(
+        new Txt('Terminos y condiciones').style('sectionHeader').end
+      );
+
+      pdf.add(
+        new Ul([
+          'La orden puede ser regresada maximo en 10 dias.',
+          'La garantia de los productos puede estar sujeta a las condiciones del productor.',
+          'Este sistema ha generado la factura.',
+        ]).end
+      );
+
+      pdf.styles({
+        sectionHeader: {
+          bold: true,
+          decoration: 'underline',
+          fontSize: 14,
+          margin: [0, 15, 0, 15]
+        }
+      });
+
+      pdf.create().open();
+    } catch (error) {
+      this.message.error('Un error ha ocurrido y no se ha generado el PDF');
+    }
+  }
+
+  addProduct(): void{
+    this.invoice.products.push(new Product());
   }
 
 }
